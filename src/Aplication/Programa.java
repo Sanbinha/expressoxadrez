ec649f68621cedfb3d58d34fedd4efc1bab64d02
Karollyne Santos Barbosa <karollynesanbarbosa@gmail.com>
2024-04-18 21:54:10 -0300

    First class position

--- src/Aplication/Programa.java
+++ src/Aplication/Programa.java
@@ -1,9 +1,12 @@
 package Aplication;
+import boardgame.Position;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position  pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
